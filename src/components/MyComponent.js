a4ec91c627ec436c474fecfd477ef0c5ed702fe4
Anthony <attran.civil@gmail.com>
2023-01-08 19:50:57 -0500

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,15 +4,18 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Anthony',
+        age: 30,
+        address: 'USA'
+    }
     // JSX
     render(){
         return(
             <div>
-                My first component
-                {Math.random()}
+                My name is {this.state.name} and my address is {this.state.address}
             </div>
         )
     }
-
 }
 export default MyComponent;
