3acc4e36d5c81c4e2cde841c43a7efda63102a6a
Anthony <attran.civil@gmail.com>
2023-01-11 06:17:59 -0500

    video 21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -14,16 +14,24 @@ class MyComponent extends React.Component {
     //     console.log('My name is ', this.state.name)
     //     this.setState({name: 'Andy',age: 25});
     // }
+
     // Arrow functions:
-    handleClick=(event)=>{
-        console.log('Click me my button');
-        console.log('My name is ', this.state.name)
-        this.setState({name: 'Andy',age: 25});
+    // handleClick=(event)=>{
+    //     this.setState({name: 'Andy',age: 25});
+    // }
+
+    handleOnchangeInput = (event)=>{
+        this.setState({name: event.target.value});
     }
     
     handleOnMouseOver(event){
         console.log(event.pageX)  
     }
+    
+    handleOnSubmit  = (event)=>{
+        event.preventDefault();
+    }
+
     // JSX
     render(){
         return(
@@ -31,6 +39,15 @@ class MyComponent extends React.Component {
                 My name is {this.state.name} and I'm {this.state.age} years old
                 <button onClick={(event)=>{this.handleClick(event)}}>Click me</button>
                 <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <form onSubmit={(event)=>{this.handleOnSubmit(event)}}>
+                    <input 
+                    type="text"
+                    onChange={(event)=>{this.handleOnchangeInput(event)}}>
+                    </input>
+                    <button>
+                        Submit
+                    </button>
+                </form>
             </div>
         )
     }
