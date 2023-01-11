91289cd6af9cb9c61ab4d67c951af528642fca7e
Anthony <attran.civil@gmail.com>
2023-01-11 05:50:03 -0500

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,11 +9,28 @@ class MyComponent extends React.Component {
         age: 30,
         address: 'USA'
     }
+    // handleClick(event){
+    //     console.log('Click me my button');
+    //     console.log('My name is ', this.state.name)
+    //     this.setState({name: 'Andy',age: 25});
+    // }
+    // Arrow functions:
+    handleClick=(event)=>{
+        console.log('Click me my button');
+        console.log('My name is ', this.state.name)
+        this.setState({name: 'Andy',age: 25});
+    }
+    
+    handleOnMouseOver(event){
+        console.log(event.pageX)  
+    }
     // JSX
     render(){
         return(
             <div>
-                My name is {this.state.name} and my address is {this.state.address}
+                My name is {this.state.name} and I'm {this.state.age} years old
+                <button onClick={(event)=>{this.handleClick(event)}}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
             </div>
         )
     }
