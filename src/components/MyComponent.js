ec4c7ae0943fd7d2b8867702ceffce5ffc8e7d72
Anthony <attran.civil@gmail.com>
2023-01-13 06:03:41 -0500

    video 24

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,12 +3,23 @@
 
 import React from "react";
 import UserInfo from "./UserInfo";
+import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
-  // JSX
+  state = {
+    listUsers: [
+      { id: "1", name: "John", email: "John@example.com", age: "30" },
+      { id: "2", name: "Eric", email: "Eric@example.com", age: "35" },
+      { id: "3", name: "Marry", email: "Marry@example.com", age: "19" },
+      { id: "4", name: "Lucas", email: "Lucas@example.com", age: "50" },
+    ],
+  };
   render() {
     return (
       <div>
         <UserInfo></UserInfo>
+        <br />
+        <br />
+        <DisplayInfo listUsers={this.state.listUsers} />
       </div>
     );
   }
