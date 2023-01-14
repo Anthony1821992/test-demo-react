0b218b0d4b0307d2f4ce0c23551c57ffa7ae2ebc
Anthony <attran.civil@gmail.com>
2023-01-14 10:20:20 -0500

    video 25

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,21 +1,40 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+  handleShowHide = () => {
+    this.setState({ isShowListUser: !this.state.isShowListUser });
+  };
   render() {
     const { listUsers } = this.props;
     return (
-      <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div> My name is {user.name}</div>
-              <div> My age is {user.age}</div>
-              <div> My email is {user.email}</div>
-              <hr />
-            </div>
-          );
-        })}
-      </div>
+      <>
+        <span
+          onClick={() => {
+            this.handleShowHide();
+          }}
+        >
+          {this.state.isShowListUser === true
+            ? "Hide list users:"
+            : "Show list users:"}
+        </span>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 30 ? "green" : "red"}>
+                  <div> My name is {user.name}</div>
+                  <div> My age is {user.age}</div>
+                  <div> My email is {user.email}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </>
     );
   }
 }
