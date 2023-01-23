ff523e2cffbc69dbdd38df5c88193c423710dc5c
Anthony <attran.civil@gmail.com>
2023-01-22 21:58:17 -0500

    video 55

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,4 +1,27 @@
+import SideBar from "./SideBar";
+import "./Admin.scss";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
 const Admin = (props) => {
-  return <div>Admin Component</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div>
+      <div className="admin-container">
+        <div className="admin-sidebar">
+          <SideBar collapsed={collapsed} />
+        </div>
+
+        <div className="admin-content">
+          <FaBars
+            onClick={() => {
+              setCollapsed(!collapsed);
+            }}
+          />
+          <span>Content Here</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Admin;
---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,102 @@
+import "react-pro-sidebar/dist/scss/styles.scss";
+import "./SideBar.scss";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+import {
+  FaTachometerAlt,
+  FaGem,
+  FaList,
+  FaGithub,
+  FaRegLaughWink,
+  FaHeart,
+} from "react-icons/fa";
+import sidebarBg from "../../assets/bg2.jpg";
+import { DiReact } from "react-icons/di";
+import { MdDashboard } from "react-icons/md";
+const SideBar = (props) => {
+  const { image, collapsed, rtl, toggled, handleToggleSidebar } = props;
+
+  return (
+    <>
+      <ProSidebar
+        image={sidebarBg}
+        rtl={rtl}
+        collapsed={collapsed}
+        toggled={toggled}
+        breakPoint="md"
+        onToggle={handleToggleSidebar}
+      >
+        <SidebarHeader>
+          <div
+            style={{
+              padding: "24px",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              fontSize: 14,
+              letterSpacing: "1px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            <DiReact size={"3em"} color={"00bfff"} />
+            <span>AndyWooTr</span>
+          </div>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <Menu iconShape="circle">
+            <MenuItem
+              icon={<MdDashboard />}
+              suffix={<span className="badge red">New</span>}
+            >
+              Dashboard
+            </MenuItem>
+          </Menu>
+          <Menu iconShape="circle">
+            <SubMenu title={"Features"} icon={<FaGem />}>
+              <MenuItem> Users Management</MenuItem>
+              <MenuItem> Quizzes Management</MenuItem>
+              <MenuItem> Questions Management</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter style={{ textAlign: "center" }}>
+          <div
+            className="sidebar-btn-wrapper"
+            style={{
+              padding: "20px 24px",
+            }}
+          >
+            <a
+              href="https://github.com/azouaoui-med/react-pro-sidebar"
+              target="_blank"
+              className="sidebar-btn"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+              <span
+                style={{
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                ViewSource
+              </span>
+            </a>
+          </div>
+        </SidebarFooter>
+      </ProSidebar>
+    </>
+  );
+};
+export default SideBar;
